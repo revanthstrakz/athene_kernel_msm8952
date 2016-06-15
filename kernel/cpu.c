c4ae0b4edc23eb3c50fd5aec4c9f2a9c1fd06f55
Thierry Strudel <tstrudel@google.com>
2016-06-14 17:46:44 -0700

    cpu: send KOBJ_ONLINE event when enabling cpus
    In case some sysfs nodes needs to be labeled with a different label than
    sysfs then user needs to be notified when a core is brought back online.
    
    MOT-CRs-fixed: IKSWN-22672
    Bug: 29359497
    Change-Id: I0395c86e01cd49c348fda8f93087d26f88557c91
    Signed-off-by: Thierry Strudel <tstrudel@google.com>
    (cherry picked from commit b36ac4e8d5df1ebf814fe352cc54d118ca88d4fa)
    
    Conflicts:
    
    	kernel/cpu.c
    
    Change-Id: Ie5a0de6f5deef6228d23d06b598e0aca75758d59
    Reviewed-on: https://gerrit.mot.com/938030
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Igor Kovalenko <igork@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- kernel/cpu.c
+++ kernel/cpu.c
@@ -562,7 +562,7 @@ void __ref enable_nonboot_cpus(void)
 	for_each_cpu(cpu, frozen_cpus) {
 		error = _cpu_up(cpu, 1);
 		if (!error) {
-			printk(KERN_INFO "CPU%d is up\n", cpu);
+			pr_info("CPU%d is up\n", cpu);
 			cpu_device = get_cpu_device(cpu);
 			if (!cpu_device)
 				pr_err("%s: failed to get cpu%d device\n",
